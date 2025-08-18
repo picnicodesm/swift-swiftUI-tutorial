21a4c039e67c09413449ee678ba69852762ebef9
Sangmin Kim <offiksm0903@naver.com>
2025-08-18 12:16:17 +0900

    finish Tutorial: ChatPrototype

--- ChatPrototype/ChatPrototype/ContentView.swift
+++ ChatPrototype/ChatPrototype/ContentView.swift
@@ -10,10 +10,43 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Knock Knock!")
+                .padding()
+                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
+            Text("Who's There?")
+                .padding()
+                .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
+
+            VStack {
+                HStack {
+                    Text("Cool")
+                        .padding()
+                        .background(Color.mint, in: RoundedRectangle(cornerRadius: 8))
+                    Text("Game")
+                        .padding()
+                        .background(Color.blue, in: RoundedRectangle(cornerRadius: 8))
+                    Text("Cook")
+                        .padding()
+                        .background(Color.red, in: RoundedRectangle(cornerRadius: 8))
+                }
+                .padding()
+                .background(Color.cyan, in: .capsule)
+                
+                HStack {
+                    Text("Together")
+                        .padding()
+                        .background(Color.gray, in: RoundedRectangle(cornerRadius: 8))
+                    Text("Movie")
+                        .padding()
+                        .background(Color.purple, in: RoundedRectangle(cornerRadius: 8))
+                    Text("Night")
+                        .foregroundStyle(Color.white)
+                        .padding()
+                        .background(Color.black, in: RoundedRectangle(cornerRadius: 8))
+                }
+                .padding()
+                .background(Color.cyan, in: .buttonBorder)
+            }
         }
         .padding()
     }
