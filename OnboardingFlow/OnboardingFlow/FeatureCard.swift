1c9167d4ea2bc1567f64bed206b4ec3e2f2b65be
Sangmin Kim <offiksm0903@naver.com>
2025-08-18 16:34:41 +0900

    feat: Add Empty Feature Card File
    - set default text of FeaturesPage

---
+++ OnboardingFlow/OnboardingFlow/FeatureCard.swift
@@ -0,0 +1,18 @@
+//
+//  FeatureCard.swift
+//  OnboardingFlow
+//
+//  Created by picnic on 8/18/25.
+//
+
+import SwiftUI
+
+struct FeatureCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeatureCard()
+}
