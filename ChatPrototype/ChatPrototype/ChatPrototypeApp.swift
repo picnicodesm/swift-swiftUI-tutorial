f4160ae7323326c8e4bd9299789e5dfc37645d90
Sangmin Kim <offiksm0903@naver.com>
2025-08-18 11:42:28 +0900

    add Tutorial: ChatPrototype

---
+++ ChatPrototype/ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by picnic on 8/18/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
