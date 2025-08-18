de3b2763ec6895ef7eefd3fdaa98e167feda85fa
Sangmin Kim <offiksm0903@naver.com>
2025-08-18 12:19:24 +0900

    add: Tutorial - WeatherForecast

---
+++ WeatherForecast/WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by picnic on 8/18/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
