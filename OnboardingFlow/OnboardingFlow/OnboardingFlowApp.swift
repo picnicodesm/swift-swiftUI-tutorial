4fe694606fc9726fe574a834f21e6938ac0169bf
Sangmin Kim <offiksm0903@naver.com>
2025-08-18 16:19:34 +0900

    add: Tutorial - ObboardingFlow

---
+++ OnboardingFlow/OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by picnic on 8/18/25.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
