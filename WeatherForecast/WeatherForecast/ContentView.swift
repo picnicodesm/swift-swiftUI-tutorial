590d7addb9ce2a3f83a8801aa4c4f23acb5264c7
Sangmin Kim <offiksm0903@naver.com>
2025-08-18 12:42:54 +0900

    refactor: Use Subview

--- WeatherForecast/WeatherForecast/ContentView.swift
+++ WeatherForecast/WeatherForecast/ContentView.swift
@@ -10,21 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            VStack {
-                Text("Mon")
-                Image(systemName: "sun.max.fill")
-                    .foregroundStyle(Color.yellow)
-                Text("High: 70")
-                Text("Low: 50")
-            }
+            DayForecast(day: "Mon", isRainy: false, high: 70, low: 50)
             
-            VStack {
-                Text("Mon")
-                Image(systemName: "cloud.rain.fill")
-                    .foregroundStyle(Color.blue)
-                Text("High: 60")
-                Text("Low: 40")
-            }
+            DayForecast(day: "Tue", isRainy: true, high: 60, low: 40)
         }
         .padding()
     }
@@ -32,4 +20,43 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct DayForecast: View {
+    let day: String
+    let isRainy: Bool
+    let high: Int
+    let low: Int
+    
+    var iconName: String {
+        if isRainy {
+            return "cloud.rain.fill"
+        } else {
+            return "sun.max.fill"
+        }
+    }
+    
+    var iconColor: Color {
+        if isRainy {
+            return Color.blue
+        } else {
+            return Color.yellow
+        }
+    }
+    
+    var body: some View {
+        // font는 의미를 포함하는 수정자이고 fontWeight은 의미를 포함하지 않는 수정자이다.
+        VStack {
+            Text(day)
+                .font(.headline)
+            Image(systemName: iconName)
+                .foregroundStyle(iconColor)
+                .font(.largeTitle)
+                .padding(5)
+            Text("High: \(high)")
+                .fontWeight(Font.Weight.semibold)
+            Text("Low: \(low)")
+                .fontWeight(Font.Weight.medium)
+        }
+    }
 }
