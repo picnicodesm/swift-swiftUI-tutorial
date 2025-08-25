6f794d468b023f1c6a7817a04ae965cc1e1ec382
Sangmin Kim <offiksm0903@naver.com>
2025-08-25 22:45:15 +0900

    add: Tutorial - PickAPal

---
+++ PickAPal/PickAPal/PickAPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickAPalApp.swift
+//  PickAPal
+//
+//  Created by picnic on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct PickAPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
