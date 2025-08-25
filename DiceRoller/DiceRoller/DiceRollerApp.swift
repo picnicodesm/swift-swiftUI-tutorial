8115bb64f130a9d7a4fee2bea670163f02a29616
Sangmin Kim <offiksm0903@naver.com>
2025-08-25 17:39:51 +0900

    add: Tutorial - DiceRoller

---
+++ DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by picnic on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
