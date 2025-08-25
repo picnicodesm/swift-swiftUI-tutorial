8de6cd327dd70f3658c5b7a55733821f875dc2c2
Sangmin Kim <offiksm0903@naver.com>
2025-08-25 18:05:24 +0900

    feat: Make default feature and UI

--- DiceRoller/DiceRoller/DiceView.swift
+++ DiceRoller/DiceRoller/DiceView.swift
@@ -8,8 +8,22 @@
 import SwiftUI
 
 struct DiceView: View {
+    @State private var numberOfPips: Int = 1
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(systemName: "die.face.\(numberOfPips)")
+                .resizable()
+                .frame(maxWidth: 100, maxHeight: 100)
+                .aspectRatio(1, contentMode: .fit)
+            
+            Button("Roll") {
+                withAnimation {
+                    numberOfPips = Int.random(in: 1...6)
+                }
+            }
+            .buttonStyle(.bordered)
+        }
     }
 }
 
