71e5ab4f4b2e7c0bc367dbb0490b021faf82f987
Sangmin Kim <offiksm0903@naver.com>
2025-08-25 17:43:07 +0900

    feat: Add DiceView(SwiftUI View) file

---
+++ DiceRoller/DiceRoller/DiceView.swift
@@ -0,0 +1,18 @@
+//
+//  DiceView.swift
+//  DiceRoller
+//
+//  Created by picnic on 8/25/25.
+//
+
+import SwiftUI
+
+struct DiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DiceView()
+}
