127e6ec8ed1a0c9cef56c4a693c81767f5207f66
Sangmin Kim <offiksm0903@naver.com>
2025-08-25 23:11:25 +0900

    feat: Improve app design

--- PickAPal/PickAPal/ContentView.swift
+++ PickAPal/PickAPal/ContentView.swift
@@ -15,14 +15,27 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            VStack(spacing: 8) {
+                Image(systemName: "person.3.sequence.fill")
+                    .foregroundStyle(.tint)
+                    .symbolRenderingMode(.hierarchical)
+                Text("Pick-a-Pal")
+            }
+            .font(.title)
+            .bold()
+            
             /* 빈 문자열 대신 단일 공백을 사용하면 텍스트 보기가 항상 화면의 세로 공간을 차지하도록 할 수 있습니다. 텍스트 보기는 콘텐츠가 없을 때 공간을 차지하지 않아 화면의 다른 보기가 이동하게 됩니다. */
             Text(pickedName.isEmpty ? " ": pickedName)
+                .font(.title2)
+                .bold()
+                .foregroundStyle(.tint)
             
             List {
                 ForEach(names, id: \.description) { name in
                     Text(name)
                 }
             }
+            .clipShape(RoundedRectangle(cornerRadius: 8))
             
             TextField("Add Name", text: $nameToAdd)
                 .autocorrectionDisabled()
