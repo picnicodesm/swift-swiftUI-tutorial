737362445a2257ee662c20f671d50f05c8b08454
Sangmin Kim <offiksm0903@naver.com>
2025-08-26 13:01:11 +0900

    feat: Make SettingsView

---
+++ ScoreKeeper/ScoreKeeper/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  ScoreKeeper
+//
+//  Created by picnic on 8/26/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
