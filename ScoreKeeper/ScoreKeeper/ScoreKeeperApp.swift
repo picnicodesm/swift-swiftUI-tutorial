127a047915611d87f3d85524dd5c6d387d8be05a
Sangmin Kim <offiksm0903@naver.com>
2025-08-26 10:34:07 +0900

    add: Tutorial - ScoreKeeper

---
+++ ScoreKeeper/ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by picnic on 8/26/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
