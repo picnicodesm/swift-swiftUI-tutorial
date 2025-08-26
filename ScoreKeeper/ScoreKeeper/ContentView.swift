c8f742f6ec99aa6873f81d349955d5e33731cb3c
Sangmin Kim <offiksm0903@naver.com>
2025-08-26 10:47:05 +0900

    refactor: Combine players and scores to Player

--- ScoreKeeper/ScoreKeeper/ContentView.swift
+++ ScoreKeeper/ScoreKeeper/ContentView.swift
@@ -8,19 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var players: [String] = ["Elisha", "Andre", "Jasmine"]
-    @State private var scores: [Int] = [0, 0, 0]
+    @State private var players: [Player] = [
+        Player(name: "Elisha", score: 0),
+        Player(name: "Andre", score: 0),
+        Player(name: "Jasmine", score: 0),
+    ]
     
     var body: some View {
         VStack {
-            ForEach(0..<players.count, id: \.description) { index in
-                TextField("Name", text: $players[index])
-                Stepper("\(scores[index])", value: $scores[index])
+            ForEach($players) { $player in
+                TextField("Name", text: $player.name)
+                Stepper("\(player.score)", value: $player.score)
             }
             
             Button("Add Player", systemImage: "plus") {
-                players.append("")
-                scores.append(0)
+                players.append(Player(name: "", score: 0))
             }
             
             Spacer()
