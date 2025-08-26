b1832adecb65964e8b58dcbcf6a3800f4fa47caa
Sangmin Kim <offiksm0903@naver.com>
2025-08-26 10:39:56 +0900

    feat: Make scoreboard

--- ScoreKeeper/ScoreKeeper/ContentView.swift
+++ ScoreKeeper/ScoreKeeper/ContentView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var players: [String] = ["Elisha", "Andre", "Jasmine"]
+    @State private var scores: [Int] = [0, 0, 0]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ForEach(0..<players.count, id: \.description) { index in
+                TextField("Name", text: $players[index])
+                Stepper("\(scores[index])", value: $scores[index])
+            }
+            
+            Button("Add Player", systemImage: "plus") {
+                players.append("")
+                scores.append(0)
+            }
+            
+            Spacer()
         }
         .padding()
     }
