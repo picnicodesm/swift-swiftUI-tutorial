21f3b79156f863581f3a23004fd4a74a7898a3cc
Sangmin Kim <offiksm0903@naver.com>
2025-08-26 12:43:58 +0900

    feat: Add Target "ScoreKeeperTests"

---
+++ ScoreKeeper/ScoreKeeperTests/ScoreKeeperTests.swift
@@ -0,0 +1,16 @@
+//
+//  ScoreKeeperTests.swift
+//  ScoreKeeperTests
+//
+//  Created by picnic on 8/26/25.
+//
+
+import Testing
+
+struct ScoreKeeperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
