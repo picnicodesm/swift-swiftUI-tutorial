4a3d0a0b946fcd495b7b10648c07bc23b2ff13f4
Sangmin Kim <offiksm0903@naver.com>
2025-08-27 16:30:22 +0900

    feat: Make FirendListView

---
+++ FriendsFavoriteMovies/FriendsFavoriteMovies/FriendList.swift
@@ -0,0 +1,18 @@
+//
+//  FriendList.swift
+//  FriendsFavoriteMovies
+//
+//  Created by picnic on 8/27/25.
+//
+
+import SwiftUI
+
+struct FriendList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FriendList()
+}
