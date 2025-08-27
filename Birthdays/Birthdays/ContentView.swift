4c49806f60d9ecad2fae0f9d045c0294434d61ff
Sangmin Kim <offiksm0903@naver.com>
2025-08-27 13:43:43 +0900

    feat: Make default list view

--- Birthdays/Birthdays/ContentView.swift
+++ Birthdays/Birthdays/ContentView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends: [Friend] = [
+        Friend(name: "Elton Lin", birthday: .now),
+        Friend(name: "Jenny Court", birthday: Date(timeIntervalSince1970: 0)),
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(friends, id: \.name) { friend in
+                HStack {
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                }
+            }
+            .navigationTitle("Birthdays")
         }
-        .padding()
     }
 }
 
