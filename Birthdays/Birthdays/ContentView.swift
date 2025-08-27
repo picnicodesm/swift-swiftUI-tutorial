187202c080a6b1745f69a902f8ccb20c2dd3b18b
Sangmin Kim <offiksm0903@naver.com>
2025-08-27 14:13:41 +0900

    feat: Apply SwiftData

--- Birthdays/Birthdays/ContentView.swift
+++ Birthdays/Birthdays/ContentView.swift
@@ -6,12 +6,11 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
-    @State private var friends: [Friend] = [
-        Friend(name: "Elton Lin", birthday: .now),
-        Friend(name: "Jenny Court", birthday: Date(timeIntervalSince1970: 0)),
-    ]
+    @Query private var friends: [Friend]
+    @Environment(\.modelContext) private var context
     
     @State private var newName = ""
     @State private var newDate = Date.now
@@ -36,7 +35,7 @@ struct ContentView: View {
                     }
                     Button("Save") {
                         let newFriend = Friend(name: newName, birthday: newDate)
-                        friends.append(newFriend)
+                        context.insert(newFriend)
                         
                         newName = ""
                         newDate = .now
@@ -46,10 +45,15 @@ struct ContentView: View {
                 .padding()
                 .background(.bar)
             }
+            .task {
+                context.insert(Friend(name: "Elton Lin", birthday: .now))
+                context.insert(Friend(name: "Jenny Court", birthday: Date(timeIntervalSince1970: 0)))
+            }
         }
     }
 }
 
 #Preview {
     ContentView()
+        .modelContainer(for: Friend.self, inMemory: true)
 }
--- Birthdays/Birthdays/Friend.swift
+++ Birthdays/Birthdays/Friend.swift
@@ -6,8 +6,16 @@
 //
 
 import Foundation
+import SwiftData
+// SwiftData는 앱의 데이터를 모델링하고 지속성을 사용하여 데이터를 저장하여 누군가가 앱을 떠날 때 사라지지 않도록 하는 도구를 제공합니다.
 
-struct Friend {
+@Model
+class Friend {
     var name: String
     var birthday: Date
+    
+    init(name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday
+    }
 }
