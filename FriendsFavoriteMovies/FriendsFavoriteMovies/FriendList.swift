f44a131fbeec419a6f473052fa4b7f5f388071e8
Sangmin Kim <offiksm0903@naver.com>
2025-08-27 16:33:07 +0900

    feat: Display firends on the ListView

--- FriendsFavoriteMovies/FriendsFavoriteMovies/FriendList.swift
+++ FriendsFavoriteMovies/FriendsFavoriteMovies/FriendList.swift
@@ -6,13 +6,26 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct FriendList: View {
+    @Query(sort: \Friend.name) private var friends: [Friend]
+    @Environment(\.modelContext) private var context
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(friends) { friend in
+                Text(friend.name)
+            }
+        }
+        .task {
+            context.insert(Friend(name: "Elena"))
+            context.insert(Friend(name: "Rody"))
+        }
     }
 }
 
 #Preview {
     FriendList()
+        .modelContainer(for: Friend.self, inMemory: true)
 }
