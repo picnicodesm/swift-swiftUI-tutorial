11f686dc3455b268fe219631551c5d72964fa529
Sangmin Kim <offiksm0903@naver.com>
2025-08-27 13:13:10 +0900

    add: Tutorial - Birthdays

---
+++ Birthdays/Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by picnic on 8/27/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
