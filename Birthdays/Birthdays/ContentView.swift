f26ebe54447765e78a74ae8f82e4b937856f458c
Sangmin Kim <offiksm0903@naver.com>
2025-08-27 13:51:35 +0900

    feat: Make saving logic and UI

--- Birthdays/Birthdays/ContentView.swift
+++ Birthdays/Birthdays/ContentView.swift
@@ -13,6 +13,9 @@ struct ContentView: View {
         Friend(name: "Jenny Court", birthday: Date(timeIntervalSince1970: 0)),
     ]
     
+    @State private var newName = ""
+    @State private var newDate = Date.now
+    
     var body: some View {
         NavigationStack {
             List(friends, id: \.name) { friend in
@@ -23,6 +26,26 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) { // .safeAreaInset은 콘텐츠를 화면의 어느 한쪽에나 고정할 수 있습니다.
+                VStack(alignment: .center, spacing: 20) {
+                    Text("New Birthday")
+                        .font(.headline)
+                    DatePicker(selection: $newDate, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newDate)
+                        friends.append(newFriend)
+                        
+                        newName = ""
+                        newDate = .now
+                    }
+                    .bold()
+                }
+                .padding()
+                .background(.bar)
+            }
         }
     }
 }
