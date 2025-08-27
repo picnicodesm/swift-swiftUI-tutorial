ebeeff639e45ff2a8e9ec86ab4500fdd063c4cda
Sangmin Kim <offiksm0903@naver.com>
2025-08-27 16:08:37 +0900

    add: Tutorial - FriendsFavoriteMovies
    [Goal]
    이 튜토리얼에서는 친구와 영화를 나열하는 앱을 만들어 사람들이 각각에 대한 세부 화면으로 이동할 수 있도록 합니다. 다음 두 튜토리얼에서는 앱을 확장하여 사람들이 기존 데이터를 추가 및 편집하고, 친구와 영화 간의 관계를 만들고, 검색 인터페이스를 구축할 수 있도록 합니다.
    
    앱의 뷰에서 SwiftData 쿼리를 사용할 것입니다. 미리보기를 위한 데이터를 제공하기 위해 샘플 데이터를 처리하는 중앙 집중식 시스템을 만들 것입니다. 그렇게 하면 뷰를 디자인할 때 뷰 코드에 집중하는 데 도움이 됩니다.

---
+++ FriendsFavoriteMovies/FriendsFavoriteMovies/FriendsFavoriteMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFavoriteMoviesApp.swift
+//  FriendsFavoriteMovies
+//
+//  Created by picnic on 8/27/25.
+//
+
+import SwiftUI
+
+@main
+struct FriendsFavoriteMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
